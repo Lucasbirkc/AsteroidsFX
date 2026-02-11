91b308526af365685cf2a157f29ac4042ac288e1
lucasbirkc <lucasbirkc@gmail.com>
2026-02-11 10:49:13 +0100

    Setup Bullet, Enemy, and Player components

---
+++ Bullet/src/main/java/dk/sdu/cbse/Bullet.java
@@ -0,0 +1,7 @@
+package dk.sdu.cbse;
+
+public class Bullet {
+    public static void main(String[] args) {
+        System.out.println("Hello, Bullet World!");
+    }
+}
---
+++ Player/src/main/java/dk/sdu/cbse/Player.java
@@ -0,0 +1,7 @@
+package dk.sdu.cbse;
+
+public class Player {
+    public static void main(String[] args) {
+        System.out.println("Hello, Player World!");
+    }
+}
