69dc72c1264aac4c6d827891e0bdd4c743fc073f
lucasbirkc <lucasbirkc@gmail.com>
2026-02-18 10:16:51 +0100

    Created basic Entity class

--- Common/src/main/java/dk/sdu/cbse/common/Entity.java
+++ Common/src/main/java/dk/sdu/cbse/common/Entity.java
@@ -1,7 +1,50 @@
 package dk.sdu.cbse.common;
 
 public class Entity {
-    public static void main(String[] args) {
-        System.out.println("Hello, Entity World!");
+
+    private double[] polygonCoordinates
+    private double posX;
+    private double posY;
+    private double rotation;
+    private double radius;
+
+    public void setPolygonCoordinates(double[] newPolygonCoordinates) {
+        this.polygonCoordinates = newPolygonCoordinates;
+    }
+
+    public void getPolygonCoordinates() {
+        return polygonCoordinates;
+    }
+
+    public void setPosX(double newPosX) {
+        this.posX = newPosX;
+    }
+
+    public void getPosX() {
+        return posX;
+    }
+
+    public void setPosY(double newPosY) {
+        this.posY = newPosY;
+    }
+
+    public void getPosY() {
+        return posY;
+    }
+
+    public void setRotation(double newRotation) {
+        this.rotation = newRotation;
+    }
+
+    public void getRotation() {
+        return rotation;
+    }
+
+    public void setRadius(double newRadius) {
+        this.radius = newRadius;
+    }
+
+    public void getRadius() {
+        return radius;
     }
 }
