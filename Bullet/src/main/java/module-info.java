995f94cab73880f8b9062f12a8a36b8200e06cd8
lucasbirkc <lucasbirkc@gmail.com>
2026-02-26 13:25:07 +0100

    Removed unnecessary module-info.java files

--- Bullet/src/main/java/module-info.java
+++
@@ -1,3 +0,0 @@
-module Bullet {
-    exports dk.sdu.cbse.bullet;
-}
--- Core/src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-module Core {
-    requires javafx.graphics;
-    exports dk.sdu.cbse.gameapp;
-    opens dk.sdu.cbse.gameapp to javafx.graphics;
-}
