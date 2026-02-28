9d877ecec6f46deb943e2983e3b1f8e0bc20bb20
lucasbirkc <lucasbirkc@gmail.com>
2026-02-28 15:38:14 +0100

    Implemented IEntityProcessorService

--- Common/src/main/java/dk/sdu/cbse/common/services/IEntityProcessorService.java
+++ Common/src/main/java/dk/sdu/cbse/common/services/IEntityProcessorService.java
@@ -1,4 +1,13 @@
 package dk.sdu.cbse.common.services;
 
+import dk.sdu.cbse.common.data.GameData;
+import dk.sdu.cbse.common.data.World;
+
 public interface IEntityProcessorService {
+    /**
+     *
+     * @param gameData
+     * @param world
+     */
+    void process(GameData gameData, World world);
 }
