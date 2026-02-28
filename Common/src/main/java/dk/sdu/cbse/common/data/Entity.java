efc573bfb92a960817d6ada5e9bea0b772d18a11
lucasbirkc <lucasbirkc@gmail.com>
2026-02-28 16:12:29 +0100

    Implemented Entity map for IEntityPart parts

--- Common/src/main/java/dk/sdu/cbse/common/data/Entity.java
+++ Common/src/main/java/dk/sdu/cbse/common/data/Entity.java
@@ -1,11 +1,32 @@
 package dk.sdu.cbse.common.data;
 
+import dk.sdu.cbse.common.parts.IEntityPart;
+
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Entity {
-    private final String ID = UUID.randomUUID().toString();
+    private final String id;
+    private final Map<Class<? extends IEntityPart>, IEntityPart> parts;
+
+    public Entity() {
+        this.id = UUID.randomUUID().toString();
+        this.parts = new ConcurrentHashMap<>();
+    }
 
     public String getID() {
-        return ID;
+        return id;
+    }
+
+    public void addPart(IEntityPart part) {
+        this.parts.put(part.getClass(), part);
+    }
+
+    public void removePart(IEntityPart part) {
+        this.parts.remove(part.getClass());
+    }
+
+    public IEntityPart getParts() {
+        return this.parts.values();
     }
 }
