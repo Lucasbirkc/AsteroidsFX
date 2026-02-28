722ccfab8e5223cf472f370e91ed2bee73b5b689
lucasbirkc <lucasbirkc@gmail.com>
2026-02-28 15:59:45 +0100

    Reduced complexity of Entity to purely ID

--- Common/src/main/java/dk/sdu/cbse/common/data/Entity.java
+++ Common/src/main/java/dk/sdu/cbse/common/data/Entity.java
@@ -1,50 +1,11 @@
 package dk.sdu.cbse.common.data;
 
+import java.util.UUID;
+
 public class Entity {
+    private final String ID = UUID.randomUUID().toString();
 
-    private double[] polygonCoordinates;
-    private double posX;
-    private double posY;
-    private double rotation;
-    private double radius;
-
-    public void setPolygonCoordinates(double[] newPolygonCoordinates) {
-        this.polygonCoordinates = newPolygonCoordinates;
-    }
-
-    public double[] getPolygonCoordinates() {
-        return polygonCoordinates;
-    }
-
-    public void setPosX(double newPosX) {
-        this.posX = newPosX;
-    }
-
-    public double getPosX() {
-        return posX;
-    }
-
-    public void setPosY(double newPosY) {
-        this.posY = newPosY;
-    }
-
-    public double getPosY() {
-        return posY;
-    }
-
-    public void setRotation(double newRotation) {
-        this.rotation = newRotation;
-    }
-
-    public double getRotation() {
-        return rotation;
-    }
-
-    public void setRadius(double newRadius) {
-        this.radius = newRadius;
-    }
-
-    public double getRadius() {
-        return radius;
+    public String getID() {
+        return ID;
     }
 }
