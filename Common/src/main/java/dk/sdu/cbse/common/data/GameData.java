4b54a695382ec452c7485c91f15c27b305412533
lucasbirkc <lucasbirkc@gmail.com>
2026-02-28 15:55:43 +0100

    Implemented GameData in Common

--- Common/src/main/java/dk/sdu/cbse/common/data/GameData.java
+++ Common/src/main/java/dk/sdu/cbse/common/data/GameData.java
@@ -1,4 +1,28 @@
 package dk.sdu.cbse.common.data;
 
 public class GameData {
+
+    private int displayHeight = 500;
+    private int displayWidth = 500;
+    private float deltaTime = 0;
+
+    public void setDisplayHeight(int height) {
+        this.displayHeight = height;
+    }
+
+    public int getDisplayHeight() {
+        return displayHeight;
+    }
+
+    public void setDisplayWidth(int width) {
+        this.displayWidth = width;
+    }
+
+    public int getDisplayWidth() {
+        return displayWidth;
+    }
+
+    public void setDeltaTime(float deltaTime) {this.deltaTime = deltaTime;}
+
+    public float getDeltaTime() { return deltaTime;}
 }
