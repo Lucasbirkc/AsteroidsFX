bb01271257187927581acb89a7c858dd038a94d6
lucasbirkc <lucasbirkc@gmail.com>
2026-03-02 10:53:30 +0100

    Implemented constructor for Game class with Entity processing lists and gamePlugin list

--- Core/src/main/java/dk/sdu/cbse/main/Game.java
+++ Core/src/main/java/dk/sdu/cbse/main/Game.java
@@ -1,16 +1,34 @@
 package dk.sdu.cbse.main;
 
+import dk.sdu.cbse.common.data.GameData;
+import dk.sdu.cbse.common.data.World;
+import dk.sdu.cbse.common.parts.IEntityPart;
+import dk.sdu.cbse.common.services.IEntityProcessorService;
+import dk.sdu.cbse.common.services.IGamePluginService;
+import dk.sdu.cbse.common.services.IPostEntityProcessorService;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-class Game {
+import java.util.List;
 
+class Game {
+    private final GameData gameData = new GameData();
+    private final World world = new World();
     private final Pane gameWindow = new Pane();
+    private final List<IEntityProcessorService> entityProcessorServices;
+    private final List<IPostEntityProcessorService> postEntityProcessorServices;
+    private final List<IGamePluginService> gamePluginServices;
+
+    Game(List<IGamePluginService> gamePluginServices, List<IEntityProcessorService> entityProcessingServices, List<IPostEntityProcessorService> postEntityProcessingServices) {
+        this.entityProcessorServices = entityProcessingServices;
+        this.postEntityProcessorServices = postEntityProcessingServices;
+        this.gamePluginServices = gamePluginServices;
+    }
 
     public void start(Stage window) throws Exception {
         System.out.println("Game starts");
-        gameWindow.setPrefSize(500, 500);
+        gameWindow.setPrefSize(gameData.getDisplayWidth(), gameData.getDisplayHeight());
 
         Scene scene = new Scene(gameWindow);
 
@@ -30,4 +48,8 @@ class Game {
     public void draw() {
 
     }
+
+    public List<IGamePluginService> getGamePluginServices() { return gamePluginServices; }
+    public List<IEntityProcessorService> getEntityProcessorServices() { return entityProcessorServices; }
+    public List<IPostEntityProcessorService> getPostEntityProcessorServices() { return postEntityProcessorServices; }
 }
