d530546ec95ad16e1466ec01fd942edca3363bd8
lucasbirkc <lucasbirkc@gmail.com>
2026-05-13 08:34:39 +0200

    Added GameKeys class and started implementing player process func

--- Player/src/main/java/dk/sdu/cbse/player/PlayerControl.java
+++ Player/src/main/java/dk/sdu/cbse/player/PlayerControl.java
@@ -1,8 +1,22 @@
 package dk.sdu.cbse.player;
 
-public class PlayerControl {
+import dk.sdu.cbse.common.data.Entity;
+import dk.sdu.cbse.common.data.GameData;
+import dk.sdu.cbse.common.data.World;
+import dk.sdu.cbse.common.services.IEntityProcessorService;
+
+public class PlayerControl implements IEntityProcessorService {
 
     public void process() {
         System.out.println("PlayerControl processed");
+    }
+
+    @Override
+    public void process(GameData gameData, World world)
+    {
+        for (Entity player : world.getEntities(Player.class))
+        {
+
+        }
     }
 }
