6e5c71cd3233cf7d30a9cf3863f1231871867f08
lucasbirkc <lucasbirkc@gmail.com>
2026-05-28 21:29:14 +0200

    Created bullet and IBulletCreator, and started implementing createBullet

--- Bullet/src/main/java/dk/sdu/cbse/bullet/BulletControl.java
+++ Bullet/src/main/java/dk/sdu/cbse/bullet/BulletControl.java
@@ -1,5 +1,29 @@
 package dk.sdu.cbse.bullet;
 
-public class BulletControl {
+import dk.sdu.cbse.common.data.Entity;
+import dk.sdu.cbse.common.data.GameData;
+import dk.sdu.cbse.common.data.World;
+import dk.sdu.cbse.common.services.IEntityProcessorService;
+import dk.sdu.cbse.commonbullet.Bullet;
+import dk.sdu.cbse.commonbullet.IBulletCreator;
 
+public class BulletControl implements IEntityProcessorService, IBulletCreator {
+
+    @Override
+    public void process(GameData gameData, World world) {
+
+    }
+
+    @Override
+    public Entity createBullet(Entity entity, GameData gameData) {
+        float bulletRadius = 2;
+
+        Entity bullet = new Bullet();
+
+        double entityRotation = Math.toRadians(entity.getRotation());
+
+        bullet.setRadius(bulletRadius);
+
+        return bullet;
+    }
 }
